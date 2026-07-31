158bb68c8cdaed4c3d930e7d1c09244c1af10211
Marc Espin <marc@mespin.me>
2026-08-01 00:30:07 +0200

    feat(core): GlobalContext context for all windows to share lazy-initialized reactive state between each other such as i18n or queries storage

--- crates/freya-winit/src/renderer.rs
+++ crates/freya-winit/src/renderer.rs
@@ -85,6 +85,7 @@ pub struct WinitRenderer {
     pub(crate) tray_icon: Option<TrayIcon>,
     pub resumed: bool,
     pub windows: FxHashMap<WindowId, AppWindow>,
+    pub global_contexts: GlobalContexts,
     pub proxy: EventLoopProxy<NativeEvent>,
     pub plugins: PluginsManager,
     pub fallback_fonts: Vec<Cow<'static, str>>,
@@ -98,6 +99,7 @@ pub struct WinitRenderer {
 
 pub struct RendererContext<'a> {
     pub windows: &'a mut FxHashMap<WindowId, AppWindow>,
+    pub global_contexts: &'a GlobalContexts,
     pub proxy: &'a mut EventLoopProxy<NativeEvent>,
     pub plugins: &'a mut PluginsManager,
     pub fallback_fonts: &'a mut Vec<Cow<'static, str>>,
@@ -118,6 +120,7 @@ impl RendererContext<'_> {
             self.font_manager,
             self.fallback_fonts,
             self.gpu_resource_cache_limit,
+            self.global_contexts,
         );
 
         let window_id = app_window.window.id();
@@ -288,6 +291,7 @@ impl ApplicationHandler<NativeEvent> for WinitRenderer {
                     &self.font_manager,
                     &self.fallback_fonts,
                     self.gpu_resource_cache_limit,
+                    &self.global_contexts,
                 );
 
                 self.proxy
@@ -351,6 +355,7 @@ impl ApplicationHandler<NativeEvent> for WinitRenderer {
                     font_manager: &mut self.font_manager,
                     font_collection: &mut self.font_collection,
                     gpu_resource_cache_limit: self.gpu_resource_cache_limit,
+                    global_contexts: &self.global_contexts,
                 };
                 (cb)(&mut renderer_context);
             }
@@ -372,6 +377,7 @@ impl ApplicationHandler<NativeEvent> for WinitRenderer {
                     fallback_fonts: &mut self.fallback_fonts,
                     active_event_loop,
                     windows: &mut self.windows,
+                    global_contexts: &self.global_contexts,
                     proxy: &mut self.proxy,
                     plugins: &mut self.plugins,
                     font_manager: &mut self.font_manager,
@@ -405,6 +411,7 @@ impl ApplicationHandler<NativeEvent> for WinitRenderer {
                             &self.font_manager,
                             &self.fallback_fonts,
                             self.gpu_resource_cache_limit,
+                            &self.global_contexts,
                         );
 
                         self.proxy
@@ -563,6 +570,7 @@ impl ApplicationHandler<NativeEvent> for WinitRenderer {
                                             &self.font_manager,
                                             &self.fallback_fonts,
                                             self.gpu_resource_cache_limit,
+                                            &self.global_contexts,
                                         );
 
                                         let window_id = app_window.window.id();
@@ -604,6 +612,7 @@ impl ApplicationHandler<NativeEvent> for WinitRenderer {
                                             fallback_fonts: &mut self.fallback_fonts,
                                             active_event_loop,
                                             windows: &mut self.windows,
+                                            global_contexts: &self.global_contexts,
                                             proxy: &mut self.proxy,
                                             plugins: &mut self.plugins,
                                             font_manager: &mut self.font_manager,
@@ -668,6 +677,7 @@ impl ApplicationHandler<NativeEvent> for WinitRenderer {
                             fallback_fonts: &mut self.fallback_fonts,
                             active_event_loop: event_loop,
                             windows: &mut self.windows,
+                            global_contexts: &self.global_contexts,
                             proxy: &mut self.proxy,
                             plugins: &mut self.plugins,
                             font_manager: &mut self.font_manager,
